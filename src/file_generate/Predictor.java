6b3d7ded85481ba614a469d9f309976e7609e129
wenjibin <787156584@qq.com>
2018-02-04 18:25:59 +0800

    添加预测器

---
+++ src/file_generate/Predictor.java
@@ -0,0 +1,22 @@
+package file_generate;
+
+import svm.svm_predict;
+
+import java.io.IOException;
+
+public class Predictor {
+
+    public void predict(String problemName){
+        String modelPath = "svmfile/model/"+problemName+"_model";
+        String resultPath = "svmfile/result/"+problemName + "_result";
+        String predictPath = "svmfile/predict/"+problemName + "_predict_scaled";
+        String args = predictPath + " " + modelPath + " " + resultPath;
+        try {
+            svm_predict.main(args.split(" "));
+            System.out.println("预测结束");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
