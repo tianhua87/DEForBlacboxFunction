0166f6ac861a4e9fec5a764d6aac5391f03a102b
wenjibin <787156584@qq.com>
2018-02-04 18:07:11 +0800

    添加model生成器

---
+++ src/file_generate/ModelGenerator.java
@@ -0,0 +1,32 @@
+package file_generate;
+
+import svm.svm_train;
+
+import java.io.*;
+
+public class ModelGenerator {
+
+    public void generateModel(String problemName){
+        String parameter = readParameters(problemName);
+        String scaleTrainFilePath = "svmfile/scale/"+problemName+"_scale";
+        String modelFilrPath = "svmfile/model/"+problemName+"_model";
+        //System.out.println(parameter);
+        String args = "-s 3 -t 2 "+parameter+" "+scaleTrainFilePath+" "+modelFilrPath;
+        try {
+            svm_train.main(args.split(" "));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readParameters(String problemName){
+        try {
+            FileInputStream fis =new FileInputStream("svmfile/parameters/"+problemName+"_para_reg");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            return br.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
