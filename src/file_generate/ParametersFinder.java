5af2e8f1f0973541e25c36cff6bf963364e9cbbd
wenjibin <787156584@qq.com>
2018-02-04 17:34:11 +0800

    添加寻找最优参数

---
+++ src/file_generate/ParametersFinder.java
@@ -0,0 +1,55 @@
+package file_generate;
+
+import java.io.*;
+
+public class ParametersFinder {
+
+    public static final String regScriptFile = "gridregression.py";
+
+    public void findRegTrainParameters(String problemName){
+        File scaleFile = new File("svmfile/scale/"+problemName+"_scale");
+        String scaleFileAbsoluPath = scaleFile.getAbsolutePath();
+        File scriptFile = new File("svmfile/script/"+regScriptFile);
+        String scriptFileAbsoluPath = scriptFile.getAbsolutePath();
+
+        String parameters = execPythonScript(scriptFileAbsoluPath,scaleFileAbsoluPath);
+        if(parameters.length()!=0){
+
+            String []p = parameters.split(" ");
+            String ps = "-c "+p[0]+" -g "+p[1]+" -p "+p[2];
+
+            String parameterFilePath = "svmfile/parameters/"+problemName+"para_reg";
+            try {
+                FileOutputStream fos = new FileOutputStream(parameterFilePath);
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+                bw.write(ps);
+                bw.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public String execPythonScript(String scriptFile, String arg){
+        String parameters = "";
+        try {
+            Process process = Runtime.getRuntime().exec("python "+ scriptFile + " "+arg);
+            System.out.println("python "+ scriptFile + " "+arg);
+            InputStreamReader ir = new InputStreamReader(process.getInputStream());
+            BufferedReader br = new BufferedReader(ir);
+            String line;
+            while((line = br.readLine())!=null){
+                //System.out.println(line);
+                if(line != null)
+                    parameters = line;
+            }
+            process.waitFor();
+            System.out.println("参数："+parameters);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return parameters;
+    }
+
+}
