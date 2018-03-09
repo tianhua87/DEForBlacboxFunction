ce890014e06d8b882d6e717bb9a39f0c4f0da3b0
wenjibin <787156584@qq.com>
2018-03-09 15:35:35 +0800

    修改训练文件的生成

--- src/file_generate/TrainFileGenerator.java
+++ src/file_generate/TrainFileGenerator.java
@@ -2,6 +2,7 @@ package file_generate;
 
 import problem.BlackBoxProblem;
 import random.DERandom;
+import utilities.NumberUtil;
 
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
@@ -9,23 +10,61 @@ import java.io.OutputStreamWriter;
 
 public class TrainFileGenerator {
 
-    final int COUNT = 1000;
+    final int COUNT = 10000;
     DERandom deRandom = new DERandom();
+//    public void trainFileGenerate(String problemName){
+//        try {
+//            Class C = Class.forName("problem."+problemName);
+//            BlackBoxProblem blackBoxProblem = (BlackBoxProblem) C.newInstance();
+//            double[] x = new double[blackBoxProblem.dim];
+//            FileOutputStream fos = new FileOutputStream("svmfile/train/"+problemName+"_train");
+//            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+//            StringBuilder line = new StringBuilder();
+//            double res;
+//            for (int i = 0; i < COUNT; i++){
+//                createRandomVector(x,blackBoxProblem);//随机生成个体
+//                res = blackBoxProblem.evaluate(x,blackBoxProblem.dim); //评价个体
+//                line.append(res);
+//                for (int j = 0 ; j < blackBoxProblem.dim; j++){
+//                    line.append(" ").append(j+1).append(":").append(x[j]);
+//                }
+//                line.append("\r\n");
+//                bw.write(line.toString());
+//                bw.flush();//重要，重要，重要
+//                line.delete(0,line.length());
+//            }
+//            if (bw != null)bw.close();
+//            if(fos != null)fos.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("--------------------"+problemName+"训练文件生成结束--------------------");
+//    }
+
     public void trainFileGenerate(String problemName){
         try {
             Class C = Class.forName("problem."+problemName);
             BlackBoxProblem blackBoxProblem = (BlackBoxProblem) C.newInstance();
-            double[] x = new double[blackBoxProblem.dim];
             FileOutputStream fos = new FileOutputStream("svmfile/train/"+problemName+"_train");
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
             StringBuilder line = new StringBuilder();
-            double res;
+
+            int dim = blackBoxProblem.dim;
+            double p[][] = new double[COUNT][dim];
+            double xmin[] = new double[dim+1];
+            double xmax[] = new double[dim+1];
+            for(int i = 0; i <= dim; i++) {
+                xmin[i] = blackBoxProblem.lowLimit;
+                xmax[i] = blackBoxProblem.highLimit;
+            }
+            NumberUtil.latin_hyp(p,COUNT,dim,xmin,xmax);
+            double res[] = new double[COUNT];
             for (int i = 0; i < COUNT; i++){
-                createRandomVector(x,blackBoxProblem);//随机生成个体
-                res = blackBoxProblem.evaluate(x,blackBoxProblem.dim); //评价个体
-                line.append(res);
-                for (int j = 0 ; j < blackBoxProblem.dim; j++){
-                    line.append(" ").append(j+1).append(":").append(x[j]);
+
+                res[i] = blackBoxProblem.evaluate(p[i],dim); //评价个体
+                line.append(res[i]);
+                for (int j = 0 ; j < dim; j++){
+                    line.append(" ").append(j+1).append(":").append(p[i][j]);
                 }
                 line.append("\r\n");
                 bw.write(line.toString());
